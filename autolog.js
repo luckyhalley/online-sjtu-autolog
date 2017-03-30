cba4a0db1f0275d3de27dd0c8675847ac1d114fc
luckyhalley <mky81@126.com>
2017-03-30 23:38:08 +0800

    Added:evaluate

--- autolog.js
+++ autolog.js
@@ -13,9 +13,9 @@ if (process.env.NODE_ENV === 'log') {
                 termidentify: "2017_1"
                 })
             .end(function(err, res){
-                    let html = res.res.text;
-                    let $ = cheerio.load(html);
-                    let lessons = $('.section-list ul li>a');
+                    let html = res.res.text,
+                        $ = cheerio.load(html),
+                        lessons = $('.section-list ul li>a');
                     lessons.each(function(index, item){
                         let lession_id = item.attribs["data-id"]
                             request
@@ -29,24 +29,55 @@ if (process.env.NODE_ENV === 'log') {
             });
     });
 } else if (process.env.NODE_ENV === 'evaluate') {
-    console.log("TODO");
+    autoCourseids.forEach((courseid)=> {
+        request
+            .get('http://www.onlinesjtu.com/learningspace/learning/student/kaoqinquery/kaoqin_list.asp')
+            .query({ 
+                courseid: courseid,
+                term_identify: "2017_1"
+                })
+            .end(function(err, res){
+                let html = res.res.text,
+                    $ = cheerio.load(html);
+                    ct = $("table").next().find("tbody tr").children().eq(1).text();
+                    request
+                        .post('http://218.1.73.12/PingJia/Default.aspx')
+                        .query({ 
+                            sid: studentId, 
+                            cid: courseid,
+                            term: "2017_1",
+                            ct
+                        })
+                        .set('Content-Type', 'application/x-www-form-urlencoded')
+                        .send({ 
+                            btsubmit: "提交评价",
+                            ddlcourse: courseid,
+                            ddlcoursetime: ct + "0:00:00",
+                            gvquestion$ctl02$hfid:1,
+                            gvquestion$ctl02$rblscore:5,
+                            gvquestion$ctl03$hfid:2,
+                            gvquestion$ctl03$rblscore:5,
+                            gvquestion$ctl04$hfid:3,
+                            gvquestion$ctl04$rblscore:5,
+                            gvquestion$ctl05$hfid:4,
+                            gvquestion$ctl05$rblscore:5,
+                            gvquestion$ctl06$hfid:5,
+                            gvquestion$ctl06$rblscore:5,
+                            rblcomment:1,
+                            tbcomment:""
+                        })
+                        .end(function(err, res){
+                            console.log("" + res.res.text);
+                        });      
+
+                console.log(ct);
+
+            });
+        
+
+    });
+
 } else {
     console.log("请运行npm run log 或 evaluate");
 }
-//http://www.onlinesjtu.com/learningspace/learning/student/kaoqinquery/kaoqin_list.asp?courseid=856&term_identify=2017_1
-//http://218.1.73.12/PingJia/Default.aspx?sid=716901010033&cid=2704&ct=2017-3-26&term=2017_1
-// btsubmit:提交评价
-// ddlcourse:2704
-// ddlcoursetime:2017-3-26 0:00:00
-// gvquestion$ctl02$hfid:1
-// gvquestion$ctl02$rblscore:5
-// gvquestion$ctl03$hfid:2
-// gvquestion$ctl03$rblscore:5
-// gvquestion$ctl04$hfid:3
-// gvquestion$ctl04$rblscore:5
-// gvquestion$ctl05$hfid:4
-// gvquestion$ctl05$rblscore:5
-// gvquestion$ctl06$hfid:5
-// gvquestion$ctl06$rblscore:5
-// rblcomment:1
-// tbcomment:
+
